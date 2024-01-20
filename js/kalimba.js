75919f05d23bd0786e6cb484cc928cfdb19f5d2d
Dunamis4tw <LeoganPro@yandex.ru>
2024-01-20 22:47:08 +0500

    added support for custom soundfonts

--- js/kalimba.js
+++ js/kalimba.js
@@ -15,6 +15,18 @@ $(document).ready(function () {
     //     "C8"
     // ];
 
+    // const allNotesFlat = [
+    //     "A0", "Bb0",
+    //     "C1", "Db1", "D1", "Eb1", "E1", "F1", "Gb1", "G1", "Ab1", "A1", "Bb1", "B1",
+    //     "C2", "Db2", "D2", "Eb2", "E2", "F2", "Gb2", "G2", "Ab2", "A2", "Bb2", "B2",
+    //     "C3", "Db3", "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3", "B3",
+    //     "C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4", "B4",
+    //     "C5", "Db5", "D5", "Eb5", "E5", "F5", "Gb5", "G5", "Ab5", "A5", "Bb5", "B5",
+    //     "C6", "Db6", "D6", "Eb6", "E6", "F6", "Gb6", "G6", "Ab6", "A6", "Bb6", "B6",
+    //     "C7", "Db7", "D7", "Eb7", "E7", "F7", "Gb7", "G7", "Ab7", "A7", "Bb7", "B7",
+    //     "C8"
+    //   ];
+
     // C4	C#4	D4	D#4	E4	F4	F#4	G4	G#4	A4	A#4	B4  - Все ноты по порядку
     // C4		D4		E4	F4		G4		A4		B4  - Ноты без смещения
     // C#4		D#4		F4	F#4		G#4		A#4		C5  - Ноты со смещением
@@ -50,16 +62,28 @@ $(document).ready(function () {
     // kalimbaOptions.init();
 
 
-
-
-
+    const Soundfonts = {
+        'FluidR3_GM': {
+            url: 'https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/kalimba-mp3.js',
+            gain: 8,
+        },
+        'FatBoy': {
+            url: 'https://gleitz.github.io/midi-js-soundfonts/FatBoy/kalimba-mp3.js',
+            gain: 8,
+        },
+        'Keylimba': {
+            url: '/soundfonts/keylimba/kalimba.mp3.js',
+            gain: 2,
+        },
+    };
 
     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
     // Загружаем звуки калимбы
-    const Kalimba = Soundfont.instrument(audioContext, 'kalimba', { gain: 10 });
+    let soundfont = window.localStorage && null !== window.localStorage.getItem("soundfont") ? window.localStorage.getItem("soundfont") : "FluidR3_GM";
+    var Kalimba = Soundfont.instrument(audioContext, Soundfonts[soundfont].url, { gain: Soundfonts[soundfont].gain });
     const kalimbaKeysContainer = $('.kalimba-keys');
-    const allNotes = [                
-                                      "A0", "B0",
+    const allNotes = [
+        "A0", "B0",
         "C1", "D1", "E1", "F1", "G1", "A1", "B1",
         "C2", "D2", "E2", "F2", "G2", "A2", "B2",
         "C3", "D3", "E3", "F3", "G3", "A3", "B3",
@@ -74,7 +98,7 @@ $(document).ready(function () {
     // Возвращает массив нот, где keys - количество клавиш, arrangement - порядок
     function getArrayNotesKalimba(keys, arrangement) {
         const posC4 = 23;
-        slicedNotes = allNotes.slice(posC4, posC4+parseInt(keys));
+        slicedNotes = allNotes.slice(posC4, posC4 + parseInt(keys));
         orderedNotes = sortArrayKalimba(slicedNotes);
 
         switch (arrangement) {
@@ -92,10 +116,10 @@ $(document).ready(function () {
     // Сортирует входящий массив клавиш и выводит их в порядке Калимбы
     function sortArrayKalimba(notesArr) {
         let sortedArr = []
-        for (let i = notesArr.length - notesArr.length%2-1; i > 0; i-=2) {
+        for (let i = notesArr.length - notesArr.length % 2 - 1; i > 0; i -= 2) {
             sortedArr.push(notesArr[i]);
         }
-        for (let i = 0; i < notesArr.length; i+=2) {
+        for (let i = 0; i < notesArr.length; i += 2) {
             sortedArr.push(notesArr[i]);
         }
         return sortedArr;
@@ -105,33 +129,33 @@ $(document).ready(function () {
     function addKeys(notesArray) {
         // Чистим поле
         kalimbaKeysContainer.empty();
+        
         // Перебираем массив с клавишами, которые надо добавить на поле
         notesArray.forEach(note => {
 
             // Получаем номер клавиши, где C4 - 0, D4 - 1 и т.д.
-            let num = convertStringToNumber(note)-28;
+            let num = convertStringToNumber(note) - 28;
             // Теперь номера 8 9 10 ... преобразовываем в 1 2 3 ...
             let labelNum = num % 7 + 1;
 
             // Определяем сколько точек нужно нарисовать сверху цифры
             dots = ""
             for (let i = 0; i < Math.floor(num / 7); i++) dots += ".";
-            
+
             // Получаем итоговую метку клавиши
             let label = dots + "\n" + labelNum;
 
+            let heightMultiplier = (27+notesArray.length)- convertStringToNumber(note);
             // Создаём элемент клавиши указав высоту и ноту
             const keyZone = $('<div>', {
                 class: 'key-zone',
                 note: note,
-                height: (360 - 5*convertStringToNumber(note)) + "px"
+                height: (150 + 5 * heightMultiplier) + "px"
             });
 
             // Создаём элемент клавиши указав высоту и ноту
             const keyElement = $('<div>', {
                 class: 'key',
-                note: note,
-                height: (350 - 5*convertStringToNumber(note)) + "px"
             });
 
             // Создаём блок с меткой клавиши
@@ -162,7 +186,7 @@ $(document).ready(function () {
         var result = firstLetterValue + (secondDigitValue * 7);
         return result;
     }
-    
+
     // Добавляем события на нажатия клавиш
     function attachEventListeners(kalimba) {
         const keys = $('.key-zone');
@@ -176,7 +200,7 @@ $(document).ready(function () {
                     keyShake($('.key', this));
                 }
             });
-            
+
             $(this).on('click', function () {
                 playSound(kalimba, note);
                 keyShake($('.key', this));
@@ -209,12 +233,12 @@ $(document).ready(function () {
                 event.preventDefault();
                 return;
             }
-            
+
             // Проверка, есть ли нота для нажатой клавиши
             if (keyNoteMap.hasOwnProperty(event.keyCode)) {
                 const baseNote = keyNoteMap[event.keyCode];
 
-                let octaveModifier=parseInt(baseNote[1]);
+                let octaveModifier = parseInt(baseNote[1]);
                 // Если пробел нажат, увеличиваем октаву на 1
                 if (isSpacePressed) octaveModifier++;
 
@@ -222,8 +246,7 @@ $(document).ready(function () {
 
                 let KeyDiv = $(`[note="${note}"]`);
                 // Если соотвествующая клавиша есть на странице
-                if (KeyDiv.length)
-                {
+                if (KeyDiv.length) {
                     // Вызов функций воспроизведения звука и анимации клавиши
                     playSound(kalimba, note);
                     keyShake(KeyDiv);
@@ -245,7 +268,7 @@ $(document).ready(function () {
 
 
     }
-    
+
     // Присваивает события на нажатия когда Калимба загрузится
     Kalimba.then(function (kalimba) {
         attachKeyboardEventListeners(kalimba);
@@ -262,7 +285,7 @@ $(document).ready(function () {
     // Воспроизводит звук
     function playSound(instrument, note) {
         instrument.play(note);
-        console.log('Pressed \'' + note + '\' (' + convertStringToNumber(note) + ')' );
+        console.log('Pressed \'' + note + '\' (' + convertStringToNumber(note) + ')');
     }
 
     // Флаг, нажата ли ЛКМ
@@ -284,38 +307,49 @@ $(document).ready(function () {
         }
     });
 
-    
+
     $('input[type=range]').on('input', function () {
         $(this).trigger('change');
     });
 
-    
-    // Событие при смене количества клавиш
-    $('#range-keys').change( function() {
-        let keys = $('#range-keys').val();
-        window.localStorage && window.localStorage.setItem("KeysCount", keys);
-        $('#range-keys-value').text(keys);
-        let arrangement = $('input:checked','#arrangement-radio-list').attr("id");
-        addKeys(getArrayNotesKalimba(keys, arrangement));
-    });
-
-    // Событие при смене Arrangement
-    $('input', '#arrangement-radio-list').on("click", function () {
-        let keys = $('#range-keys').val();
-        let arrangement = $('input:checked','#arrangement-radio-list').attr("id");
-        window.localStorage && window.localStorage.setItem("Arrangement", arrangement);
-        addKeys(getArrayNotesKalimba(keys, arrangement));
-    });
 
     // Получаем количество клавиш из localStorage и отображаем на странице
     let keysCount = window.localStorage && null !== window.localStorage.getItem("KeysCount") ? window.localStorage.getItem("KeysCount") : 17;
     $('#range-keys').val(keysCount);
     $('#range-keys-value').text(keysCount);
 
+    // Событие при смене количества клавиш
+    $('#range-keys').change(function () {
+        keysCount = $('#range-keys').val();
+        window.localStorage && window.localStorage.setItem("KeysCount", keysCount);
+        $('#range-keys-value').text(keysCount);
+        addKeys(getArrayNotesKalimba(keysCount, arrangement));
+    });
+
+
     // Получаем порядок клавиш из localStorage и отображаем на странице
     let arrangement = window.localStorage && null !== window.localStorage.getItem("Arrangement") ? window.localStorage.getItem("Arrangement") : "Alternating";
     $("input#" + arrangement).prop('checked', true);
-    
+
+    // Событие при смене Arrangement
+    $('input', '#arrangement-radio-list').on("click", function () {
+        arrangement = $('input:checked', '#arrangement-radio-list').attr("id");
+        window.localStorage && window.localStorage.setItem("Arrangement", arrangement);
+        addKeys(getArrayNotesKalimba(keysCount, arrangement));
+    });
+
+    $('#soundfonts').val(soundfont);
+
+    // Событие при смене Soundfont
+    $('#soundfonts').change(function () {
+        var soundfont = $(this).val();
+        console.log('Current Soundfont: ' + soundfont);
+        Kalimba = Soundfont.instrument(audioContext, Soundfonts[soundfont].url, { gain: Soundfonts[soundfont].gain });
+        addKeys(getArrayNotesKalimba(keysCount, arrangement));
+
+        window.localStorage && window.localStorage.setItem("soundfont", soundfont);
+    });
+
     // Создаём Калимбу с полученными значениями
     addKeys(getArrayNotesKalimba(keysCount, arrangement));
 });
